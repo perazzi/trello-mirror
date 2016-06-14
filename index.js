49b97c144b38f9fb2cb78519e4cefe94b60bf7d8
Andre Perazzi Afonso <aperazzi@us.ibm.com>
2016-06-14 15:23:41 -0400

    skeleton

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/card/change',function(req,res){
 	console.log(req.body)
 })
 
-app.get('/card/change',function(req,res){
+app.head('/card/change',function(req,res){
 	res.status(200).end()
 })
 
